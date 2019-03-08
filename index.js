6a401e0c211cf27ff5f28838ebc4d83ac3a8f173
jokerdelta <jokerdelta@yahoo.com>
2019-03-07 19:58:38 -0800

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function increaseRankBy(n) {
+  const list =  document.getElementById('app').querySelectorAll('ul.ranked-list li');
+   for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = (parseInt(list[i].innerHTML) + n);
+   }
+}
+
+function deepestChild(){
+  const list =  document.getElementById('grand-node').querySelectorAll('div');
+  return list[(list.length - 1)];
+
+}
